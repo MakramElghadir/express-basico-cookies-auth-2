fdbe42d9b87e1cc2dc3d327ade34e2ba4f4057c5
MakramElghadir <MkElghadir@gmail.com>
2024-11-25 10:53:00 +0100

    added and corrected request.put

--- test.py
+++ test.py
@@ -16,8 +16,19 @@ print("[TEST] GET /api/users/admin =>", r.status_code)
 r = requests.post(url + "/api/login", data={"user": "admin", "password": "admin"})
 print("[TEST] POST /api/login =>", r.status_code, r.cookies)
 
-r =requests.put (url + "/api/users", data={"user": "admin", "password": "admin"})
-print("[TEST] PUT /api/users =>", r.status_code)
+user_obj = { 
+    "password": "nuevacontraseña"
+}
+
+r =requests.put (url + "/api/users/admin", data= user_obj)
+print("[TEST] PUT /api/users =>", r.status_code, r.text)
+
+lista = [1,2,3]
+
+valor 1 = "hola"
+valor 2 = "mundo"
 
 cookie = r.cookies
 user = cookie.get("user")
+
+
