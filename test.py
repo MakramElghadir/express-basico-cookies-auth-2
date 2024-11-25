cb067df64b43ba82f7fa6d8970b96034e405b5dc
MakramElghadir <MkElghadir@gmail.com>
2024-11-25 10:40:22 +0100

    Añadir request.put en test.py

--- test.py
+++ test.py
@@ -15,5 +15,9 @@ print("[TEST] GET /api/users/admin =>", r.status_code)
 
 r = requests.post(url + "/api/login", data={"user": "admin", "password": "admin"})
 print("[TEST] POST /api/login =>", r.status_code, r.cookies)
+
+r =requests.put (url + "/api/users", data={"user": "admin", "password": "admin"})
+print("[TEST] PUT /api/users =>", r.status_code)
+
 cookie = r.cookies
 user = cookie.get("user")
